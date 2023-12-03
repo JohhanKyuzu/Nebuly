b1285d6c08b5b56d30542a80594376b7735e07cb
JohhanKyuzu <152097693+JohhanKyuzu@users.noreply.github.com>
2023-12-03 17:30:24 -0300

    level 1-1 inicio + prefabs

--- scripts/porta.gd
+++ scripts/porta.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
-@export var destino := ([0,0])
+#@export var destino := ([0,0])
 
 
 #func transport(player):
 #	player.position.x = destino[0]
 #	player.position.y = destino[1]
 
+@export var destino : String = ""
 func transport(_player):
-	get_tree().change_scene_to_file("res://level/world_1-1.tscn")
+	get_tree().change_scene_to_file(destino)
